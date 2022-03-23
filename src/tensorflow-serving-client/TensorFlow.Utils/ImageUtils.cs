9beacb5067ddff3c59ce2a637115dfcfce3eaaa9
박영석 <yeongseok.park@mirero.co.kr>
2022-03-23 15:59:14 +0900

    Utils 추가

---
+++ src/tensorflow-serving-client/TensorFlow.Utils/ImageUtils.cs
@@ -0,0 +1,53 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
+
+namespace TensorFlow.Utils
+{
+    public class ImageUtils
+    {
+        public Image ResizeImage(Image image, int width, int height)
+        {
+            image.Mutate(x => x.Resize(width, height));
+            return image;
+        }
+
+        public byte[] ImageToByteArray(Image image, IImageFormat format)
+        {
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                image.Save(memoryStream, format);
+                return memoryStream.ToArray();
+            }
+        }
+
+        
+
+        public float[][][] ConvertImageStreamToDimArrays(int numRows, int numCols, int numChannels, MemoryStream stream)
+        {
+            var imageMatrix = new float[numRows][][];
+            for (int row = 0; row < numRows; row++)
+            {
+                imageMatrix[row] = new float[numCols][];
+                for (int col = 0; col < numCols; col++)
+                {
+                    imageMatrix[row][col] = new float[numChannels];
+                    for (int channel = 0; channel < numChannels; channel++)
+                    {
+                        imageMatrix[row][col][channel] = stream.ReadByte();
+                    }
+                }
+            }
+            return imageMatrix;
+        }
+        
+
+        public (Image, IImageFormat) FromImageFile(string imagePath)
+        {
+            IImageFormat format = null;
+            Image image = Image.Load(imagePath, out format);
+            return (image, format);
+        }
+    }
+}
---
+++ src/tensorflow-serving-client/TensorFlow.Utils/TensorBuilder.cs
@@ -0,0 +1,58 @@
+﻿
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tensorflow;
+
+namespace TensorFlow.Utils
+{
+    public class TensorBuilder
+    {
+        public static TensorProto CreateTensorFromImage(Image image, IImageFormat format, float revertsBits = 1.0f)
+        {
+			int width = Convert.ToInt32(image.Width);
+			int height = Convert.ToInt32(image.Height);
+			var imageUtils = new ImageUtils();
+
+
+			var bytes = imageUtils.ImageToByteArray(image, format);
+
+			var memoryStream = new MemoryStream();
+			memoryStream.Write(bytes);
+
+			var dimArray = imageUtils.ConvertImageStreamToDimArrays(width, height, 3, memoryStream);
+            return CreateTensorFromImage(dimArray, revertsBits, dimArray.Length, dimArray[0].Length, dimArray[0][0].Length);
+        }
+
+		public static TensorProto CreateTensorFromImage(float[][][] dimArray, float revertsBits, int height, int width, int channels)
+		{
+			var imageFeatureShape = new TensorShapeProto();
+
+			imageFeatureShape.Dim.Add(new TensorShapeProto.Types.Dim() { Size = 1 });
+			imageFeatureShape.Dim.Add(new TensorShapeProto.Types.Dim() { Size = height });
+			imageFeatureShape.Dim.Add(new TensorShapeProto.Types.Dim() { Size = width });
+			imageFeatureShape.Dim.Add(new TensorShapeProto.Types.Dim() { Size = channels });
+
+			var imageTensorBuilder = new TensorProto();
+			imageTensorBuilder.Dtype = DataType.DtFloat;
+			imageTensorBuilder.TensorShape = imageFeatureShape;
+
+			for (int i = 0; i < height; ++i)
+			{
+				for (int j = 0; j < width; ++j)
+				{
+					for (int c = 0; c < channels; c++)
+					{
+						imageTensorBuilder.FloatVal.Add(dimArray[i][j][c] / 255.0F);
+					}
+				}
+			}
+
+			return imageTensorBuilder;
+		}
+	}
+}
